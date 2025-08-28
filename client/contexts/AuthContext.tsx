bd9c089d0ee6c62b1e0947ce59cadecdf7db148f
Alish Jung Shahi <160869929+AlishJungShahi@users.noreply.github.com>
2025-08-28 07:57:21 +0545

    AuthContext updated

--- client/contexts/AuthContext.tsx
+++ client/contexts/AuthContext.tsx
@@ -110,7 +110,7 @@ export function AuthProvider({ children }: { children: ReactNode }) {
 export function useAuth() {
   const context = useContext(AuthContext);
   if (context === undefined) {
-    throw new Error('useAuth must be used within an AuthProvider');
+    throw new Error('useAuth context must be used within an AuthProvider');
   }
   return context;
 }
